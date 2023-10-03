719f573df8141573dcd00eed9e6bb7e520ac608e
starboy <124185505+new-Begginings@users.noreply.github.com>
2023-10-03 17:56:14 +0530

    Implement article model.

---
+++ models/article.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const connection = require("../config/db_connection");
+
+const articleSchema = new Schema({
+  author: { type: Schema.Types.ObjectId, ref: "user", required: true },
+  comments: [{ type: Schema.Types.ObjectId, ref: "comment" }],
+  createdAt: { type: Date, default: Date.now() },
+  title: { type: String, required: true, maxLength: 100 },
+  claps: { type: Number, default: 0 },
+  content: { type: String, required: true, maxLength: 10000 },
+});
+
+const article = connection.model("article", articleSchema);
+
+module.exports = article;
