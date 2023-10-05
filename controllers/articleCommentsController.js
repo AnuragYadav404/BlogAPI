c23b824e47d827292d3462efa0a6d0936a2397db
starboy <124185505+new-Begginings@users.noreply.github.com>
2023-10-05 13:26:18 +0530

    Implement articlCommentController for articl-comment routes.
    articleCommentController handles all the handling related
    to the comments of an particle article

---
+++ controllers/articleCommentsController.js
@@ -0,0 +1,39 @@
+const asyncHandler = require("express-async-handler");
+
+exports.get_all_comments_byArticleID = asyncHandler(async function (
+  req,
+  res,
+  next
+) {
+  res.json({
+    msg: "This sends back comments for a particular article",
+  });
+});
+
+exports.create_new_comment_byArticleID = asyncHandler(async function (
+  req,
+  res,
+  next
+) {
+  res.json({
+    msg: "This creates a new comments for a particular article.",
+  });
+});
+
+exports.get_comment = asyncHandler(async function (req, res, next) {
+  res.json({
+    msg: "This sends back a particular comment for a particular article",
+  });
+});
+
+exports.update_comment = asyncHandler(async function (req, res, next) {
+  res.json({
+    msg: "This updates a particular comment for a particular article",
+  });
+});
+
+exports.delete_comment = asyncHandler(async function (req, res, next) {
+  res.json({
+    msg: "This deletes a particular comment for a particular article.",
+  });
+});
--- routes/articleRouter.js
+++ routes/articleRouter.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const article_controller = require("../controllers/articleController");
-
+const article_comment_controller = require("../controllers/articleCommentsController");
 /* GET all the articles page. */
 
 // all articles handler
@@ -21,38 +21,33 @@ router.put("/:articleID", article_controller.update_article_byID);
 router.delete("/:articleID", article_controller.delete_article_byID);
 
 // this gets all the comments for a article
-router.get("/:articleID/comments", function (req, res, next) {
-  res.json({
-    msg: "This sends back comments for a particular article",
-  });
-});
+router.get(
+  "/:articleID/comments",
+  article_comment_controller.get_all_comments_byArticleID
+);
 
 // this creates a new comment for a particular article
-router.post("/:articleID/comments", function (req, res, next) {
-  res.json({
-    msg: "This creates a new comments for a particular article.",
-  });
-});
+router.post(
+  "/:articleID/comments",
+  article_comment_controller.create_new_comment_byArticleID
+);
 
 // this gets a particular comment for a particular article
-router.get("/:articleID/comments/:commentID", function (req, res, next) {
-  res.json({
-    msg: "This sends back a particular comment for a particular article",
-  });
-});
+router.get(
+  "/:articleID/comments/:commentID",
+  article_comment_controller.get_comment
+);
 
 // this updates a particular comment for a particular article
-router.put("/:articleID/comments/:commentID", function (req, res, next) {
-  res.json({
-    msg: "This updates a particular comment for a particular article",
-  });
-});
+router.put(
+  "/:articleID/comments/:commentID",
+  article_comment_controller.update_comment
+);
 
 // this deletes a particular comment for a particular article
-router.delete("/:articleID/comments/:commentID", function (req, res, next) {
-  res.json({
-    msg: "This deletes a particular comment for a particular article.",
-  });
-});
+router.delete(
+  "/:articleID/comments/:commentID",
+  article_comment_controller.delete_comment
+);
 
 module.exports = router;
