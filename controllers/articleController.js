258d8aa27664685ec245998a97301f2b423f7806
starboy <124185505+new-Begginings@users.noreply.github.com>
2023-10-05 15:35:41 +0530

    Implements create_article handler for creating new articles

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -28,9 +28,31 @@ exports.get_articles_list = asyncHandler(async function (req, res, next) {
 });
 
 exports.create_article = asyncHandler(async function (req, res, next) {
-  res.json({
-    msg: "This route handles a new post creation",
-  });
+  // create article is a post request to create a new article
+  // the article form info will be stored in req.context.body
+  // the author info will be associated with req.context.user
+  const existArticle = await article.find({ aid: req.context.body.aid }).exec();
+  if (existArticle) {
+    //this means that the article already exists
+    return res.json({
+      msg: "Article aldready exists",
+      href: existArticle.url,
+    });
+  } else {
+    // we will now create an article
+    const newArticle = new article({
+      aid: req.context.body.aid,
+      author: req.context.user,
+      comments: [],
+      title: req.context.body.title,
+      content: req.context.body.content,
+    });
+    await newArticle.save();
+    return res.json({
+      msg: "Article created successfull",
+      href: newArticle.url,
+    });
+  }
 });
 
 exports.get_article_byID = asyncHandler(async function (req, res, next) {
